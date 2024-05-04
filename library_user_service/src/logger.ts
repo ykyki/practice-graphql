1cbe7144d752f160676b8d113a32f6acf9be374f
ykyki <bsyki@pm.me>
2024-05-04 20:24:41 +0900

    lus: logger message is fixed

--- library_user_service/src/logger.ts
+++ library_user_service/src/logger.ts
@@ -2,6 +2,6 @@ import bunyan from "bunyan";
 
 const logger = bunyan.createLogger({ name: "_" });
 logger.addSerializers({ req: (req) => ({ method: req.method, url: req.url }) });
-logger.info("Bun server starting...");
+logger.info("logger initialized");
 
 export default logger;
